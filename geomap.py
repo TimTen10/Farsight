5517f6d0d9b647b9bf67929419661d9c578a41bb
TimTen10 <Tim.Eckle94@gmail.com>
2022-03-25 15:32:04 +0100

    Changed terrain_function

--- geomap.py
+++ geomap.py
@@ -9,6 +9,8 @@ class GeoMap:
 
         # TODO: still kind of hard coded, needs change
         # later on this is going to be a real map anyway
-        self.terrain_function = lambda x, y: abs(x - 500 + y - 500)
+        # self.terrain_function = lambda x, y: abs(x - 500 + y - 500)
+        self.terrain_function = lambda x, y: 0.5 * np.sin(0.01 * x) + 0.5 * np.cos(0.01 * y)
+
         self.height_map =\
             np.array([[self.terrain_function(dim1, dim2) for dim1 in range(shape[1])] for dim2 in range(shape[0])])
