91e5c988a69ae7247711800a90dbd296bc98b83a
TimTen10 <Tim.Eckle94@gmail.com>
2022-03-25 15:32:57 +0100

    Created function header for later

--- farsight.py
+++ farsight.py
@@ -8,8 +8,14 @@ def main(shape, distance):
     return gm.height_map
 
 
+def compute_bresenham_predecessor(height_map):
+    # returns np.array of shape height_map.shape with predecessor
+    pass
+
+
 def visualize(height_map):
-    plt.imshow(height_map, cmap='terrain')  # alternative: cmap="gist_earth"
+    plt.imshow(height_map, cmap="terrain")  # alternative: cmap="gist_earth"
+    # plt.imshow(height_map, cmap="tab20c", alpha=0.5)
     plt.show()
 
 
